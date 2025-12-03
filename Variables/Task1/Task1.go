69bee505858bb746999f4a5e82cda43782ca59a4
gen0cid <britwin.gennadij@yandex.ru>
2025-12-03 21:22:04 +0300

    Эта программа объявляет переменные разными способами (с инициализацией и без), присваивает им значения базовых типов (int, string, float64, bool) и выводит их имена и значения в консоль, демонстрируя, что неинициализированные переменные получают нулевые значения соответствующих типов.

---
+++ Variables/Task1/Task1.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	number1 := 15
+	text1 := "привет"
+	drob1 := 3.14
+	boolean1 := false
+
+	var number2 int = 13
+	var text2 string = "Hello"
+	var drob2 float64 = 14.16
+	var boolean2 bool = true
+
+	var number3 int
+	var text3 string
+	var drob3 float64
+	var boolean3 bool
+
+	fmt.Println("number1:", number1)
+	fmt.Println("text1:", text1)
+	fmt.Println("drob1:", drob1)
+	fmt.Println("boolean1:", boolean1)
+	fmt.Println("number2:", number2)
+	fmt.Println("text2:", text2)
+	fmt.Println("drob2:", drob2)
+	fmt.Println("boolean2:", boolean2)
+	fmt.Println("number3:", number3)
+	fmt.Println("text3:", text3)
+	fmt.Println("drob3:", drob3)
+	fmt.Println("boolean3:", boolean3)
+
+}
