e570367aba5d55673bf06f0ff617974e0f793b99
gen0cid <britwin.gennadij@yandex.ru>
2025-12-09 01:22:56 +0300

    chore: добавлен простой пример вызова функций frazi() и slova() для демонстрации модульности

---
+++ Functions/Task1/Task1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Привет, ")
+	frazi()
+	fmt.Println("Твое имя... ")
+	slova()
+	fmt.Println("Ах точно!!")
+}
+func frazi() {
+	fmt.Println("как дела?")
+}
+func slova() {
+	fmt.Println("Николай")
+}
