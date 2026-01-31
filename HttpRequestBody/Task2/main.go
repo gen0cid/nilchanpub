0d9d4d0e1406d3bc09ea25f50adf415679ffd5dd
gen0cid <britwin.gennadij@yandex.ru>
2026-01-31 16:58:27 +0300

    feat: реализован HTTP-сервер с in-memory базой данных для управления именами по ID — добавлены эндпоинты /Name (создание записи) и /Delete (удаление по ID), данные хранятся в мапе, сервер запускается на порту :9091

---
+++ HttpRequestBody/Task2/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+type dataBase struct {
+	names  map[int]string
+	nextId int
+}
+
+func newDataBase() *dataBase {
+	return &dataBase{
+		names:  make(map[int]string),
+		nextId: 1,
+	}
+}
+
+func (d *dataBase) handlerName(w http.ResponseWriter, r *http.Request) {
+	httRequuestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("неорректный ввод:", err)
+		return
+	}
+
+	httRequuestBodyString := string(httRequuestBody)
+
+	d.names[d.nextId] = httRequuestBodyString
+	fmt.Printf("Имя: %s зарегистрировано на id: %d\n", httRequuestBodyString, d.nextId)
+	d.nextId++
+
+}
+
+func (d *dataBase) handlerDelete(w http.ResponseWriter, r *http.Request) {
+	httRequuestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("неорректный ввод:", err)
+		return
+	}
+
+	httRequuestBodyString := string(httRequuestBody)
+
+	httRequuestId, err := strconv.Atoi(httRequuestBodyString)
+	if err != nil {
+		fmt.Println("неорректный ввод, введите id:", err)
+		return
+	}
+
+	fmt.Printf("Удаление элемента c id:%d\n", httRequuestId)
+	delete(d.names, httRequuestId)
+	fmt.Println("текущая мапа:", d.names)
+}
+
+func main() {
+	db := newDataBase()
+
+	http.HandleFunc("/Name", func(w http.ResponseWriter, r *http.Request) {
+		db.handlerName(w, r)
+	})
+
+	http.HandleFunc("/Delete", func(w http.ResponseWriter, r *http.Request) {
+		db.handlerDelete(w, r)
+	})
+
+	err := http.ListenAndServe(":9091", nil)
+
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	}
+}
